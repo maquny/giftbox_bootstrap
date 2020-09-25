35ebdf7006fc44eedecd815d1ee8ba5d631edd64
maquny <sdfun3@gmail.com>
2020-09-25 18:47:31 +0900

    123

--- src/components/containerArea.vue
+++ src/components/containerArea.vue
@@ -13,10 +13,10 @@
 			                <swiper-slide v-for="item in items" :v-Key="item">
 			                	<div class="sell_tbl">
 			                		<div class="sell_td" secondary>{{item.day}}</div>
-				                	<div class="sell_td"><span class="brand"><img :src="item.sellerBrand"></span> {{item.name}}<span>{{item.price}} 원</span></div>
+				                	<div class="sell_td"><span class="brand"><img :src="item.sellerBrand"></span> {{item.name}}<span class="won">{{item.price}} 원</span></div>
 				                	<div class="sell_td">{{item.priceRequest}}</div>
-				                	<div class="sell_td">{{item.requestComplete}}</div>
-				                	<div class="sell_td"><span>{{item.state}}</span></div>
+				                	<div class="sell_td time_num"><span>{{item.requestComplete}}</span> 초</div>
+				                	<div class="sell_td"><span :class="'state_'+item.state"></span></div>
 			                	</div>
 			                </swiper-slide>
 			            </swiper>
@@ -36,7 +36,7 @@
 				    	<div class="line"><b-form-input v-model="moneyPrice" placeholder="" @click="zeroNum()"></b-form-input></div>
 				    </div>
 				    <div class="calu_tbl p-3">
-				    	<div class="calu_tr">
+				    	<div class="calu_tr p-3">
 				    		<div class="calu_td">
 				    			<div>판매 요청 금액 :</div>
 				    			<div>{{moneyPrice}} 원</div>
@@ -97,22 +97,22 @@
 				bankTransfer: 500,
 				itemsName: ['날자', '판매자신청정보', '판매요청', '입금완료', '처리상태'],
 				items: [
-					{ day: '09.22', sellerBrand: require('@/assets/culture.jpg'), name: '홍*동', price: '100,000', priceRequest: '20.09.22', requestComplete: '1초', state: '처리중' },
-					{ day: '09.22', sellerBrand: require('@/assets/happy.jpg'), name: '홍*동', price: '100,000', priceRequest: '20.09.22', requestComplete: '1초', state: '처리중' },
-					{ day: '09.22', sellerBrand: require('@/assets/happy.jpg'), name: '홍*동', price: '100,000', priceRequest: '20.09.22', requestComplete: '1초', state: '처리중' },
-					{ day: '09.22', sellerBrand: require('@/assets/happy.jpg'), name: '홍*동', price: '100,000', priceRequest: '20.09.22', requestComplete: '1초', state: '처리중' },
-					{ day: '09.22', sellerBrand: require('@/assets/culture.jpg'), name: '홍*동', price: '100,000', priceRequest: '20.09.22', requestComplete: '1초', state: '처리중' },
-					{ day: '09.22', sellerBrand: require('@/assets/happy.jpg'), name: '홍*동', price: '100,000', priceRequest: '20.09.22', requestComplete: '1초', state: '처리중' },
-					{ day: '09.22', sellerBrand: require('@/assets/culture.jpg'), name: '홍*동', price: '100,000', priceRequest: '20.09.22', requestComplete: '1초', state: '처리중' },
-					{ day: '09.22', sellerBrand: require('@/assets/culture.jpg'), name: '홍*동', price: '100,000', priceRequest: '20.09.22', requestComplete: '1초', state: '처리중' },
-					{ day: '09.22', sellerBrand: require('@/assets/culture.jpg'), name: '홍*동', price: '100,000', priceRequest: '20.09.22', requestComplete: '1초', state: '처리중' },
-					{ day: '09.22', sellerBrand: require('@/assets/happy.jpg'), name: '홍*동', price: '100,000', priceRequest: '20.09.22', requestComplete: '1초', state: '처리중' },
-					{ day: '09.22', sellerBrand: require('@/assets/happy.jpg'), name: '홍*동', price: '100,000', priceRequest: '20.09.22', requestComplete: '1초', state: '처리중' },
-					{ day: '09.22', sellerBrand: require('@/assets/happy.jpg'), name: '홍*동', price: '100,000', priceRequest: '20.09.22', requestComplete: '1초', state: '처리중' },
-					{ day: '09.22', sellerBrand: require('@/assets/happy.jpg'), name: '홍*동', price: '100,000', priceRequest: '20.09.22', requestComplete: '1초', state: '처리중' },
-					{ day: '09.22', sellerBrand: require('@/assets/happy.jpg'), name: '홍*동', price: '100,000', priceRequest: '20.09.22', requestComplete: '1초', state: '처리중' },
-					{ day: '09.22', sellerBrand: require('@/assets/culture.jpg'), name: '홍*동', price: '100,000', priceRequest: '20.09.22', requestComplete: '1초', state: '처리중' },
-					{ day: '09.22', sellerBrand: require('@/assets/culture.jpg'), name: '홍*동', price: '100,000', priceRequest: '20.09.22', requestComplete: '1초', state: '처리중' },
+					{ day: '09.22', sellerBrand: require('@/assets/culture.jpg'), name: '홍*동', price: '100,000', priceRequest: '20.09.22', requestComplete: '1', state: 'A' },
+					{ day: '09.22', sellerBrand: require('@/assets/happy.jpg'), name: '홍*동', price: '100,000', priceRequest: '20.09.22', requestComplete: '1', state: 'A' },
+					{ day: '09.22', sellerBrand: require('@/assets/happy.jpg'), name: '홍*동', price: '100,000', priceRequest: '20.09.22', requestComplete: '1', state: 'B' },
+					{ day: '09.22', sellerBrand: require('@/assets/happy.jpg'), name: '홍*동', price: '100,000', priceRequest: '20.09.22', requestComplete: '1', state: 'A' },
+					{ day: '09.22', sellerBrand: require('@/assets/culture.jpg'), name: '홍*동', price: '100,000', priceRequest: '20.09.22', requestComplete: '1', state: 'B' },
+					{ day: '09.22', sellerBrand: require('@/assets/happy.jpg'), name: '홍*동', price: '100,000', priceRequest: '20.09.22', requestComplete: '1', state: 'A' },
+					{ day: '09.22', sellerBrand: require('@/assets/culture.jpg'), name: '홍*동', price: '100,000', priceRequest: '20.09.22', requestComplete: '1', state: 'B' },
+					{ day: '09.22', sellerBrand: require('@/assets/culture.jpg'), name: '홍*동', price: '100,000', priceRequest: '20.09.22', requestComplete: '1', state: 'A' },
+					{ day: '09.22', sellerBrand: require('@/assets/culture.jpg'), name: '홍*동', price: '100,000', priceRequest: '20.09.22', requestComplete: '1', state: 'A' },
+					{ day: '09.22', sellerBrand: require('@/assets/happy.jpg'), name: '홍*동', price: '100,000', priceRequest: '20.09.22', requestComplete: '1', state: 'A' },
+					{ day: '09.22', sellerBrand: require('@/assets/happy.jpg'), name: '홍*동', price: '100,000', priceRequest: '20.09.22', requestComplete: '1', state: 'B' },
+					{ day: '09.22', sellerBrand: require('@/assets/happy.jpg'), name: '홍*동', price: '100,000', priceRequest: '20.09.22', requestComplete: '1', state: 'B' },
+					{ day: '09.22', sellerBrand: require('@/assets/happy.jpg'), name: '홍*동', price: '100,000', priceRequest: '20.09.22', requestComplete: '1', state: 'B' },
+					{ day: '09.22', sellerBrand: require('@/assets/happy.jpg'), name: '홍*동', price: '100,000', priceRequest: '20.09.22', requestComplete: '1', state: 'A' },
+					{ day: '09.22', sellerBrand: require('@/assets/culture.jpg'), name: '홍*동', price: '100,000', priceRequest: '20.09.22', requestComplete: '1', state: 'A' },
+					{ day: '09.22', sellerBrand: require('@/assets/culture.jpg'), name: '홍*동', price: '100,000', priceRequest: '20.09.22', requestComplete: '1', state: 'A' },
 				],
 				swiperOptions: {
 					slidesPerView: 6,
@@ -167,49 +167,76 @@
 </script>
 
 <style>
-body{font-size:13px;}
 .container_box{}
-.ma_title{padding:15px;border:solid 1px #ddd;box-shadow:0 3px 6px 0 rgba(0,0,0,0.06);color:#444;font-weight:700;}
+.ma_title{padding:15px;border:solid 1px #ddd;box-shadow:0 3px 6px 0 rgba(0,0,0,0.06);color:#444;font-weight:700;position:relative;}
+.ma_title .more{position:absolute;top:50%;right:10px;width:21px;height:21px;margin-top:-10px;border:solid 1px #999;}
+.ma_title .more:before{top:9px;right:5px;width:9px;height:1px;}
+.ma_title .more:after{top:5px;right:9px;width:1px;height:9px;}
+.ma_title .more:before,
+.ma_title .more:after{content:'';display:block;clear:both;position:absolute;background-color:#444;}
 .swiper_box{border:solid 1px #ddd;border-top:none;box-shadow:0 3px 6px 0 rgba(0,0,0,0.06);}
-.swiper_container_box{height:291px;box-shadow:0 3px 6px 0 rgba(0,0,0,0.06);position:relative;}
+.swiper_container_box{height:309px;box-shadow:0 3px 6px 0 rgba(0,0,0,0.06);position:relative;}
 .swiper_blind{position:absolute;top:0;left:0;bottom:0;width:100%;height:100%;z-index:100;}
 .swiper-container {width:100%;height:100%;}
 .swiper-slide{text-align:center;font-size:18px;background:#fff;display:-webkit-box;display:-ms-flexbox;display:-webkit-flex;display:flex;-webkit-box-pack:center;-ms-flex-pack:center;-webkit-justify-content:center;justify-content:center;-webkit-box-align:center;-ms-flex-align:center;-webkit-align-items:center;align-items:center}
 .calculator_box{border:solid 1px #ddd;border-top:none;box-shadow:0 3px 6px 0 rgba(0,0,0,0.06);}
 .calculator_box .form-group{margin-bottom:0;}
-.custom-control-label{padding:2px 0;font-size:13px;}
+.custom-control-label{padding:2px 0;font-size:.8em;}
 .calu_radio{}
 .calu_radio .bv-no-focus-ring{text-align:center;padding:8px 0}
 .calu_radio .bv-no-focus-ring .custom-control{display:inline-block;margin:0 10px;text-align:center;}
 .calu_moneysum{padding:0 15px;}
 .calu_moneysum:after{content:'';display:block;clear:both;}
-.calu_moneysum .line{float:left;width:50%;text-align:left;line-height:34px;font-size:13px;}
+.calu_moneysum .line{float:left;width:50%;text-align:left;line-height:34px;font-size:.8em;}
 .calu_moneysum .line:first-child + .line{text-align:right;}
-.calu_moneysum .line .form-control{font-size:13px;text-align:right;}
+.calu_moneysum .line .form-control{font-size:.8em;text-align:right;}
 .calu_tbl{}
 .calu_tbl:after,
 .calu_tbl .calu_tr:after{content:'';display:block;clear:both;}
-.calu_tbl .calu_tr{background-color:#f3f3f3;padding:16px 20px;border-radius:7px;}
-.calu_tbl .calu_td{float:left;width:100%;padding:10px 0;}
-.calu_tbl .calu_td div{float:left;width:50%;text-align:left;font-size:13px;}
+.calu_tbl .calu_tr{background-color:#f3f3f3;border-radius:7px;}
+.calu_tbl .calu_td{float:left;width:100%;padding:13px 0;}
+.calu_tbl .calu_td div{float:left;width:50%;text-align:left;font-size:.8em;}
 .calu_tbl .calu_td div:first-child + div{text-align:right;}
 .calu_tbl .calu_td.last_td{margin-top:10px;border:solid 1px #eee;padding:7px;border-radius:7px;background-color: #fff;}
 .calu_tbl .calu_td.last_td div:first-child + div{}
 .calu_tbl .calu_td.last_td div:first-child + div span{color:#4384f3;font-weight:700;}
-.sell_tbl{width:100%;font-size:12px;}
+.sell_tbl{width:100%;font-size:.8em;}
 .sell_tbl:after{content:'';display:block;clear:both;}
 .sell_tbl .sell_th{float:left;width:19%;line-height:40px;text-align:center;border-bottom:solid 1px #ddd;background-color:#000;color:#fff;font-weight:700;}
 .sell_tbl .sell_td{float:left;width:19%;text-align:center;color:#777;}
 .sell_tbl .sell_td .brand{float:left;height:20px;margin-right:10px;}
-.sell_tbl .sell_td .brand img{height:100%;}
+.sell_tbl .sell_td .brand img{float:left;height:100%;}
+.sell_tbl .sell_td .won{padding-left:5px;}
+.sell_tbl .sell_td.time_num span{font-weight:700;}
 .sell_tbl .sell_td:first-child + .sell_td{text-align:left;}
 .sell_tbl .sell_th:first-child + .sell_th,
 .sell_tbl .sell_td:first-child + .sell_td{width:24%;}
+.state_A{}
+.state_A:after{content:'처리중';background-color:#ccc;}
+.state_B{}
+.state_B:after{content:'입금완료';background-color:#4384f3;}
+.state_A:after,
+.state_B:after{display:block;clear:both;width:80px;padding:3px 0;text-align:center;color:#fff;font-size:.8em;border-radius:8px;display:inline-block}
 
 .faq_container{padding:20px;border:solid 1px #ddd;border-top:none;box-shadow:0 3px 6px 0 rgba(0,0,0,.06)}
 .faq_container:after{content:'';display:block;clear:both}
-.faq_container .list{float:left;width:100%;position:relative;padding-right:90px;font-size:13px;line-height:30px}
+.faq_container .list{float:left;width:100%;position:relative;padding-right:90px;font-size:.8em;line-height:30px;color:#444;}
 .faq_container .list div{width:100%;text-overflow:ellipsis;white-space:nowrap;overflow:hidden}
 .faq_container .list .day{position:absolute;top:0;right:0}
 
+@media screen and (max-width: 760px){
+	.swiper_container_box{}
+	.custom-control-label,
+	.calu_moneysum .line .form-control,
+	.calu_tbl .calu_td div,
+	.sell_tbl,
+	.faq_container .list,
+	.state_A:after,
+	.state_B:after,
+	.calu_moneysum .line{font-size:.68em;}
+	.sell_td.time_num{width:10%;}
+	.sell_tbl .sell_td:first-child + .sell_td{width:33%}
+	.state_A:after,
+	.state_B:after{width:50px;}
+}
 </style>
